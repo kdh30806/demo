6c6af9b08518c4a536087b0c6de3c3d57196e702
kdh30806 <kdh30806@naver.com>
2022-06-25 16:59:54 +0900

    demo 프로젝트 oracle 연결 세팅

---
+++ demo/src/main/java/com/hyuni/demo/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.hyuni.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("data", "hello");
+		return "main";
+	}
+}
