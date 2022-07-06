08f323d09c0b150ea45ad6e4fca5d838ead113d3
kdh30806 <kdh30806@naver.com>
2022-07-06 22:59:09 +0900

    Feat: 로그인, 회원가입 모달창 디자인 완료

--- demo/src/main/resources/static/js/header.js
+++ demo/src/main/resources/static/js/header.js
@@ -2,5 +2,31 @@ $( document ).ready(function() {
     $('.logo').click(function(){
 		location.href = "/";
 	})
+	
+	$('#join').click(function(){
+		$(".modal-wrap").css("display", "inline-block");
+		$(".join-modal").css("display", "inline-block");
+	})
+	
+	$('#login').click(function(){
+		$(".modal-wrap").css("display", "inline-block");
+		$(".login-modal").css("display", "inline-block");
+	})
+	
+	$('.modal-close-btn').click(function(){
+		$(".modal-wrap").css("display", "none");
+		$(".join-modal").css("display", "none");
+		$(".login-modal").css("display", "none");
+	})
+	
+	$('.goLogin').click(function(){
+		$(".join-modal").css("display", "none");
+		$(".login-modal").css("display", "inline-block");
+	})
+	
+	$('.goJoin').click(function(){
+		$(".join-modal").css("display", "inline-block");
+		$(".login-modal").css("display", "none");
+	})
 });
 
